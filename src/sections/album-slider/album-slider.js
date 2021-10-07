e58a2d3da08d41ac0b995ef435861185673b48e8
Evan Ford <evankerrickford@gmail.com>
2021-10-07 10:47:02 -0400

    Album slider

---
+++ src/sections/album-slider/album-slider.js
@@ -0,0 +1,92 @@
+class AlbumSlider extends HTMLElement {
+  constructor() {
+    super();
+    this.el = this;
+
+    this.paginationEl = null;
+    this.prevEl = null;
+    this.nextEl = null;
+
+    this.pagination = this.getAttribute('data-pagination');
+    if (this.pagination == "progress" || this.pagination == "dots") {
+      this.paginationEl = this.el.querySelector('[data-pagination]')
+    }
+
+    this.navigation = this.getAttribute('data-navigation') == 'true';
+    if (this.navigation) {
+      this.prevEl = this.el.querySelector('[data-prev]');
+      this.nextEl = this.el.querySelector('[data-next]');
+    }
+
+    this.autoplay = this.getAttribute('data-autoplay');
+
+    this.fullWidth = this.getAttribute('data-full-width') == 'true';
+    if (typeof window.Slider == 'function' && typeof window.Slider.constructor == 'function') {
+      this.init();
+    } else {
+      window.addEventListener('slidersReady', this.init.bind(this));
+      this.init();
+    }
+  }
+
+  getParams() {
+    let params = {
+      slidesPerView: 2,
+      spaceBetween: 12,
+      autoplay: false,
+      effect: "slide",
+      watchVisibility: true,
+      lazy: true,
+      preloadImages: false,
+      centerInsufficientSlides: true,
+      breakpoints: {
+        420: {
+          // slidesPerView: 2,
+          spaceBetween: 20
+        },
+        1000: {
+          // slidesPerView: 3,
+          spaceBetween: 25,
+        },
+        1300: {
+          spaceBetween: 32,
+        },
+      },
+    };
+
+    if (this.autoplay && this.autoplay != 0)  {
+       params.autoplay = {
+        delay: this.autoplay,
+        disableOnInteraction: true,
+        pauseOnMouseEnter: true
+      }
+    }
+    if (this.navigation && this.prevEl && this.nextEl) {
+       params.navigation = {
+        prevEl: this.prevEl,
+        nextEl: this.nextEl,
+      };
+    }
+    if (this.pagination && this.pagination != 'disabled') {
+      params.pagination = {
+        type: this.pagination,
+        el: this.paginationEl,
+        dynamicBullets: true,
+      }
+    }
+    if (this.fullWidth) {
+      params.slidesPerView = 1;
+      params.breakpoints = null;
+    } else {
+      params.slidesPerView = 'auto'
+      params.breakpoints = null
+    }
+    return params;
+  }
+  init () {
+    this.params = this.getParams();
+    this.slider = new window.Slider(this, this.params)
+  }
+}
+
+customElements.define('album-slider',  AlbumSlider);
